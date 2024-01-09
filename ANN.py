9bdaa64969132242e65ef7ca17ecc0154fbf9baa
Fabian Groen <fabiangroen@live.nl>
2024-01-09 21:25:00 +0100

    3

--- ANN.py
+++ ANN.py
@@ -6,53 +6,59 @@ import torch.optim as optimizers
 import pandas as pd
 
 #Path to csv file
-dataPath = "archive/DatsetFraud.csv"
+dataPath = "archive/creditcard.csv"
 
 #Read dataset, delete IsFlaggedFraud
-dataset = pd.read_csv(dataPath)
-dataset = pd.DataFrame(dataset, columns=["step",
-                                         "type",
-                                         "amount",
-                                         "nameOrig",
-                                         "oldbalanceOrig",
-                                         "newbalanceOrig",
-                                         "nameDest",
-                                         "oldbalanceDest",
-                                         "newbalanceDest",
-                                         "isFraud"
-                                         ]) 
-print(dataset)
-
+#dataset = pd.read_csv(dataPath)
+#dataset = pd.DataFrame(dataset, columns=["step",
+#                                         "type",
+#                                         "amount",
+#                                         "nameOrig",
+#                                         "oldbalanceOrig",
+#                                         "newbalanceOrig",
+#                                         "nameDest",
+#                                         "oldbalanceDest",
+#                                         "newbalanceDest",
+#                                          "isFraud"
+#                                         ]) 
 #Split into input to the network and labels
-inputData =  pd.DataFrame(dataset, columns=["step",
-                                         "type",
-                                         "amount",
-                                         "nameOrig",
-                                         "oldbalanceOrig",
-                                         "newbalanceOrig",
-                                         "nameDest",
-                                         "oldbalanceDest",
-                                         "newbalanceDest",
-                                         ]) 
-Labels = pd.DataFrame(dataset, columns=["isFraud"])
+#inputData =  pd.DataFrame(dataset, columns=["step",
+#                                         "type",
+#                                         "amount",
+#                                         "nameOrig",
+#                                         "oldbalanceOrig",
+#                                         "newbalanceOrig",
+#                                         "nameDest",
+#                                         "oldbalanceDest",
+#                                         "newbalanceDest",
+#                                         ]) 
+#Labels = pd.DataFrame(dataset, columns=["isFraud"])
 
-print(Labels)
-print(input)
+# load the dataset, split into input (X) and output (y) variables
+dataset = np.loadtxt(dataPath, delimiter=',')
+input = dataset[:,0:10]
+labels = dataset[:,10]
 
 #Convert to tensor
 input = torch.tensor(input, dtype=torch.float32)
-output = torch.tensor(Labels, dtype=torch.float32).reshape(-1, 1)
+labels = torch.tensor(labels, dtype=torch.float32).reshape(-1, 1)
 
 model = nn.Sequential(
-    nn.Linear(9, 16),
+    nn.Linear(30, 64),
     nn.ReLU(),
-    nn.Linear(16, 32),
+    nn.Linear(64, 128),
+    nn.ReLU(),
+    nn.Linear(64, 32),
     nn.ReLU(),
     nn.Linear(32, 16),
     nn.ReLU(),
     nn.Linear(16, 8),
     nn.ReLU(),
-    nn.Linear(8, 1),
+    nn.Linear(8, 4),
+    nn.ReLU(),
+    nn.Linear(4, 2),
+    nn.ReLU(),
+    nn.Linear(2, 1),
     nn.Sigmoid())
 
 loss_fn = nn.BCELoss()  #Binary cross entropy loss
@@ -65,7 +71,7 @@ for epoch in range(n_epochs):
     for i in range(0, len(input), batch_size):
         inputBatch = input[i:i+batch_size]
         output_prediction = model(inputBatch)
-        outputBatch = output[i:i+batch_size]
+        outputBatch = labels[i:i+batch_size]
         loss = loss_fn(output_prediction, outputBatch)
         optimizer.zero_grad()
         loss.backward()
