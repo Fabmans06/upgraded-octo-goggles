9c013518931d38247c1a794be09f4a4fecac02ed
FlyingCatCreatures <117092071+FlyingCatCreatures@users.noreply.github.com>
2024-01-09 19:12:13 +0100

    comments

--- ANN.py
+++ ANN.py
@@ -1,11 +1,14 @@
+#Import utils
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import pandas as pd
 
+#Path to csv file
 dataPath = "archive/DatsetFraud.csv"
 
+#Read dataset, delete IsFlaggedFraud
 dataset = pd.read_csv(dataPath)
 dataset = pd.DataFrame(dataset, columns=["step",
                                          "type",
@@ -20,6 +23,7 @@ dataset = pd.DataFrame(dataset, columns=["step",
                                          ]) 
 print(dataset)
 
+#Split into input to the network and labels
 output =  pd.DataFrame(dataset, columns=["step",
                                          "type",
                                          "amount",
@@ -35,5 +39,6 @@ input = pd.DataFrame(dataset, columns=["isFraud"])
 print(output)
 print(input)
 
+#Convert to tensor
 input = torch.tensor(input, dtype=torch.float32)
 output = torch.tensor(output, dtype=torch.float32).reshape(-1, 1)
